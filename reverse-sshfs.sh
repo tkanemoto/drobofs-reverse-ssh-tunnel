16e65fb3fbe02991d582d49e30bb02aa6b8959cf
Takeshi Kanemoto <tak.kanemoto@gmail.com>
2015-08-31 22:47:58 +0900

    Update reverse-sshfs.sh

--- reverse-sshfs.sh
+++ reverse-sshfs.sh
@@ -7,12 +7,6 @@ remote_mountpoint=$3
 share_name=$4
 drobo_user=$5
 
-[ -n "$remote_server ] || exit 1
-[ -n "$remote_port" ] || remote_port=14443
-[ -n "remote_mountpoint" ] || remote_mountpoint=share/
-[ -n "share_name" ] || share_name=share
-[ -n "drobo_user" ] || drobo_user=root
-
 while true ; do
   ssh -R $remote_port:localhost:22 $remote_server \
     "sshfs -p $remote_port $drobo_user@localhost:/mnt/DroboFS/Shares/$share_name $remote_mountpoint"
