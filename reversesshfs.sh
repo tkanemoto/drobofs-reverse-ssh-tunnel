02dc0e9418994b8a3a4508e781406f0fb9dfffa6
Takeshi Kanemoto <tak.kanemoto@gmail.com>
2015-09-01 10:47:15 +0900

    Use sudo to run sshfs

--- reversesshfs.sh
+++ reversesshfs.sh
@@ -4,10 +4,9 @@ echo $$ > `dirname \`realpath $0\``/reversesshfs.pid
 . `dirname \`realpath $0\``/config.source
 
 ssh $remote_server "sudo mkdir -p $remote_mountpoint"
-ssh $remote_server "sudo chown ubuntu:ubuntu $remote_mountpoint"
 
 while true ; do
   ssh -R $remote_port:localhost:22 $remote_server \
-    "sshfs -p $remote_port $extra_args $drobo_user@localhost:/mnt/DroboFS/Shares/$share_name $remote_mountpoint"
+    "sudo sshfs -p $remote_port $extra_args $drobo_user@localhost:/mnt/DroboFS/Shares/$share_name $remote_mountpoint"
   sleep 10
 done
